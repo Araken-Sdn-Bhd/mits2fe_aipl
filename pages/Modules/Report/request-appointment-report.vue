776e7db2d0916a2da54d44c970c3a603ef90447a
Amir Hamid <103908024+Amiramor95@users.noreply.github.com>
2022-10-03 11:14:01 +0800

    Fixed Report: No. 10

--- pages/Modules/Report/request-appointment-report.vue
+++ pages/Modules/Report/request-appointment-report.vue
@@ -64,6 +64,10 @@
                 ref="result"
                 style="background: #fff"
               >
+              <div class="topnav"> <div>PERIOD OF SERVICES:  {{ fromDate }}</div> <p class='to'>TO</p> <div>{{ toDate }}</div> </div>
+              
+                <div>TOTAL REQUEST:  {{ total }}</div>
+
                 <table class="table" id="datatable">
                   <thead>
                     <tr>
@@ -71,24 +75,20 @@
                       <th class="thhead">Name</th>
                       <th class="thhead">NRIC/Passport</th>
                       <th class="thhead">Address</th>
-                      <th class="thhead">City</th>
-                      <th class="thhead">State</th>
-                      <th class="thhead">PostCode</th>
                       <th class="thhead">Phone Number</th>
                       <th class="thhead">Email</th>
+                      <th class="thhead">Request Date</th>
                     </tr>
-                  </thead>
+                   </thead>
                   <tbody>
                     <tr v-for="(rp, index) in list" :key="index">
                       <td class="tdrow">{{ index + 1 }}</td>
-                      <td class="tdrow">{{ rp.Name }}</td>
-                      <td class="tdrow">{{ rp.NRIC_NO_PASSPORT_NO }}</td>
-                      <td class="tdrow">{{ rp.ADDRESS }}</td>
-                      <td class="tdrow">{{ rp.CITY }}</td>
-                      <td class="tdrow">{{ rp.STATE }}</td>
-                      <td class="tdrow">{{ rp.POSTCODE }}</td>
-                      <td class="tdrow">{{ rp.PHONE_NUMBER }}</td>
-                      <td class="tdrow">{{ rp.EMAIL }}</td>
+                      <td class="tdrow">{{ rp.name }}</td>
+                      <td class="tdrow">{{ rp.nric_or_passportno  }}</td>
+                      <td class="tdrow">{{ rp.address }}</td>
+                      <td class="tdrow">{{ rp.contact_no }}</td>
+                      <td class="tdrow">{{ rp.email  }}</td>
+                      <td class="tdrow">{{ getFormattedDate(rp.created_at) }}</td>
                     </tr>
                   </tbody>
                 </table>
@@ -96,9 +96,12 @@
 </div>
 </template>
 <script>
+import moment from 'moment';
+
 import CommonHeader from '../../../components/CommonHeader.vue';
 import CommonSidebar from '../../../components/CommonSidebar.vue';
 export default {
+ 
   components: { CommonSidebar, CommonHeader },
 
   name: "sharp",
@@ -118,10 +121,17 @@ export default {
       toDate: "",
       error: null,
       list: [],
+      total:'',
+
+        
     };
   },
   beforeMount() {
     this.userdetails = JSON.parse(localStorage.getItem("userdetails"));
+    if(this.userdetails){
+      this.id=this.userdetails.user.id; 
+      this.email=this.userdetails.user.email;
+    }
   },
   methods: {
     async Ongeneratepdf() {
@@ -137,11 +147,12 @@ export default {
           fromDate: this.fromDate,
           toDate: this.toDate,
         },
-        { headers }
+        { headers, params: {email: this.email}  }
       );
       console.log("my report", response.data);
       if (response.data.code == 200) {
         this.list = response.data.result;
+        this.total = this.list.length;
         console.log("my report", response.data);
         if (response.data.filepath) {
           setTimeout(() => {
@@ -187,8 +198,22 @@ export default {
         }
       } catch (e) {}
     },
+    getFormattedDate(date) {
+            return moment(date).format("YYYY-MM-DD hh:mm:ss A")
+        },
+
+    formatDate(dateString) {
+            const date = dayjs(dateString);
+                // Then specify how you want your dates to be formatted
+            return date.format('dddd MMMM D, YYYY');
+        }
+
+
+
   },
 };
+
+
 </script>
 <style scoped>
 .tdrow {
@@ -226,6 +251,15 @@ export default {
   background: #fff;
   display: none;
 }
+.topnav .to{
+  padding-right: 10px;
+  padding-left:  10px;
+}
+
+.topnav{
+  display:flex;
+}
+
 div#result {
   padding: 35px;
 }
