5a4f5f603c99dc18431dd2db7250575f6234b828
AcapXasyraF <acapxasyrafx@yahoo.com>
2022-11-21 16:12:13 +0800

    added edit page for CD 39,41

---
+++ pages/Modules/Intervention/edit-internal-referral-form.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="layoutSidenav">
+    <CommonSidebar />
+    <div id="layoutSidenav_content">
+      <CommonHeader />
+      <main>
+        <Loader v-if="loader" />
+      </main>
+    </div>
+  </div>
+</template>
+<script>
+import CommonHeader from '../../../components/CommonHeader.vue';
+import CommonSidebar from '../../../components/CommonSidebar.vue';
+  export default {
+    components: { CommonSidebar, CommonHeader },
+    name: "",
+  data() {
+    return {
+      userdetails: null,
+      list: [],
+      branchlist: [],
+      servicelist: [],
+      keyword: "",
+      search: "",
+      branch_id: 0,
+      service_id: 0,
+      loader: true,
+      assistancelist: [],
+      SidebarAccess:null,
+    };
+  },
+  beforeMount() {
+    this.userdetails = JSON.parse(localStorage.getItem("userdetails"));
+    this.SidebarAccess = JSON.parse(localStorage.getItem("SidebarAccess"));
+  },
+  }
+</script>
