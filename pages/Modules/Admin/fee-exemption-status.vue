8da624124b69d19f00664c337d752ebedd1aeb4a
MOHDHAZIZI <82504629+MOHDHAZIZI@users.noreply.github.com>
2022-12-20 18:24:35 +0800

    pat30.12

--- pages/Modules/Admin/fee-exemption-status.vue
+++ pages/Modules/Admin/fee-exemption-status.vue
@@ -32,7 +32,7 @@
                           type="text"
                           class="form-control"
                           placeholder="Enter Fee Exemption Status"
-                          v-model="status"
+                          v-model="feestatus"
                         />
                       </div>
 
@@ -45,6 +45,13 @@
                           v-model="index"
                         />
                       </div>
+                      <div class="col-sm-3 mb-4">
+                                    <label for="" class="form-label">Status</label>
+                                    <select class="form-select" v-model="status">
+                                    <option value="1">Enable</option>
+                                    <option value="0">Disable</option>
+                                    </select>
+                                </div>
                     </div>
                       <p v-if="errorList.length">
                           <ul>
@@ -75,24 +82,27 @@
                   >
                     <thead>
                       <tr>
-                        <th>No</th>
+                        <th style="width:3%">No</th>
                         <th>Fee Exemption Status</th>
                         <th>Index</th>
-                        <th>Action</th>
+                        <th>Status</th>
+                        <th style="width:3%">Action</th>
                       </tr>
                     </thead>
                   <tbody>
                          <tr v-for="(setting, index) in settinglist" :key="index">
-                    <td>{{index+1}}</td>
+                    <td>#{{index+1}}</td>
                         <td>{{setting.section_value}}</td>
                         <td>{{setting.section_order}}</td>
+                        <td>
+                            <p v-if="setting.status == 0" style="color:red">Disabled</p>
+                            <p v-if="setting.status == 1">Enabled</p>
+                        </td>
                         <td id="tdaction" ref="tdaction" class="td"  :class="SidebarAccess!=1?'hide':''">
                           <a  class="edit" @click="editsetting(setting)"
                             ><i class="fa fa-edit"></i
                           ></a>
-                          <a class="action-icon icon-danger" @click="deletesetting(setting)"
-                            ><i class="fa fa-trash-alt"></i
-                          ></a>
+                         
                         </td>
                       </tr>
                     </tbody>
@@ -113,7 +123,7 @@ export default {
   name: "fee-exemption-status",
   data() {
     return {
-      status: "",
+      feestatus: "",
       index: 0,
       errorList: [],
       userdetails: null,
@@ -122,9 +132,11 @@ export default {
       requesttype: "insert",
       loader: false,
       SidebarAccess: null,
+      status: 1,
     };
   },
   mounted() {
+    this.loader = true;
     const headers = {
       Authorization: "Bearer " + this.userdetails.access_token,
       Accept: "application/json",
@@ -140,22 +152,7 @@ export default {
       )
       .then((resp) => {
         this.settinglist = resp.data.list;
-        $(document).ready(function () {
-          $(".data-table").DataTable({
-            searching: false,
-            bLengthChange: false,
-            bInfo: false,
-            // autoWidth: false,
-            // responsive: true,
-            scrollX: true,
-            language: {
-              paginate: {
-                next: '<i class="fad fa-arrow-to-right"></i>', // or '→'
-                previous: '<i class="fad fa-arrow-to-left"></i>', // or '←'
-              },
-            },
-          });
-        });
+        this.loader = false;
       })
       .catch ((err) => {
         this.loader = false;
@@ -197,10 +194,11 @@ export default {
             {
               added_by: this.userdetails.user.id,
               section: "fee-exemption-status",
-              section_value: this.status,
+              section_value: this.feestatus,
               section_order: this.index,
               setting_id: this.settingId,
               request_type: this.requesttype,
+              status: this.status,
             },
             { headers }
           );
@@ -215,7 +213,8 @@ this.$swal.fire(
             }
             this.GetSettingList();
             this.index = 0;
-            this.status = "";
+            this.feestatus = "";
+            this.status= 1;
             this.settingId = 0;
             this.requesttype = "insert";
           } else {
@@ -248,31 +247,7 @@ this.$swal.fire(
         this.settinglist = [];
       }
     },
-    async deletesetting(data) {
-      const headers = {
-        Authorization: "Bearer " + this.userdetails.access_token,
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      };
-      const response = await this.$axios.post(
-        "/general-setting/remove",
-        {
-          added_by: this.userdetails.user.id,
-          setting_id: data.id,
-        },
-        { headers }
-      );
-      if (response.data.code == 200) {
-        this.$swal.fire('Deleted Successfully', '', 'success');
-        this.GetSettingList();
-      } else {this.$swal.fire({
-                  icon: 'error',
-                  title: 'Oops... Something Went Wrong!',
-                  text: 'the error is: ' + JSON.stringify(response.data.message),
-                  footer: ''
-                });
-      }
-    },
+  
     async editsetting(data) {
       const headers = {
         Authorization: "Bearer " + this.userdetails.access_token,
@@ -288,8 +263,9 @@ this.$swal.fire(
       );
       if (response.data.code == 200) {
         this.settingId = response.data.setting[0].id;
-        this.status = response.data.setting[0].section_value;
+        this.feestatus = response.data.setting[0].section_value;
         this.index = response.data.setting[0].section_order;
+        this.status =response.data.setting[0].status;
         this.requesttype = "update";
       } else {
         this.$swal.fire({
