b72bfbc950cc378bfbc8d5eac2e9d96cef2ababe
MOHDHAZIZI <82504629+MOHDHAZIZI@users.noreply.github.com>
2022-12-20 18:49:48 +0800

    pat 30.14

--- pages/Modules/Admin/gender.vue
+++ pages/Modules/Admin/gender.vue
@@ -8,7 +8,6 @@
         <div class="container-fluid px-4">
           <div class="page-title">
             <h1>General Setting</h1>
-            <!-- <a href="#"><i class="fal fa-plus"></i> Add</a> -->
           </div>
             <div class="card mb-4">
               <div class="card-header icon-title">
@@ -42,6 +41,14 @@
                           v-model="index"
                         />
                       </div>
+                      
+                      <div class="col-sm-3 mb-4">
+                                    <label for="" class="form-label">Status</label>
+                                    <select class="form-select" v-model="status">
+                                    <option value="1">Enable</option>
+                                    <option value="0">Disable</option>
+                                    </select>
+                                </div>
                     </div>
                      <p v-if="errorList.length">
                           <ul>
@@ -72,24 +79,27 @@
                   >
                     <thead>
                       <tr>
-                        <th>No</th>
+                        <th style="width:3%">No</th>
                         <th>Gender</th>
                         <th>Index</th>
-                        <th>Action</th>
+                        <th>Status</th>
+                        <th style="width:3%">Action</th>
                       </tr>
                     </thead>
                 <tbody>
                          <tr v-for="(setting, index) in settinglist" :key="index">
-                          <td>{{index+1}}</td>
+                          <td>#{{index+1}}</td>
                         <td>{{setting.section_value}}</td>
                         <td>{{setting.section_order}}</td>
+                        <td>
+                                            <p v-if="setting.status == 0" style="color:red">Disabled</p>
+                                            <p v-if="setting.status == 1">Enabled</p>
+                                        </td>
                         <td class="td"  :class="SidebarAccess!=1?'hide':''">
                           <a  class="edit" @click="editsetting(setting)"
                             ><i class="fa fa-edit"></i
                           ></a>
-                          <a class="action-icon icon-danger" @click="deletesetting(setting)"
-                            ><i class="fa fa-trash-alt"></i
-                          ></a>
+                         
                         </td>
                       </tr>
                     </tbody>
@@ -118,7 +128,8 @@ export default {
       settingId: 0,
       requesttype: "insert",
       loader: false,
-      SidebarAccess:null
+      SidebarAccess:null,
+      status: 1,
     };
   },
   beforeMount() {
@@ -126,6 +137,7 @@ export default {
     this.SidebarAccess = JSON.parse(localStorage.getItem("SidebarAccess"));
   },
   mounted() {
+    this.loader = true;
     const headers = {
       Authorization: "Bearer " + this.userdetails.access_token,
       Accept: "application/json",
@@ -141,23 +153,7 @@ export default {
       )
       .then((resp) => {
         this.settinglist = resp.data.list;
-        $(document).ready(function () {
-          $(".data-table").DataTable({
-            searching: false,
-            bLengthChange: false,
-            bInfo: false,
-            // autoWidth: false,
-            // responsive: true,
-            scrollX: true,
-            pagingType: "simple",
-            language: {
-              paginate: {
-                next: '<i class="fad fa-arrow-to-right"></i>', // or '→'
-                previous: '<i class="fad fa-arrow-to-left"></i>', // or '←'
-              },
-            },
-          });
-        });
+        this.loader = false;
       })
       .catch ((err) => {
         this.loader = false;
@@ -200,6 +196,7 @@ export default {
               section_order: this.index,
               setting_id: this.settingId,
               request_type: this.requesttype,
+              status: this.status,
             },
             { headers }
           );
@@ -215,6 +212,7 @@ this.$swal.fire(
             this.GetSettingList();
             this.index = 0;
             this.gender = "";
+            this.status= 1;
             this.settingId = 0;
             this.requesttype = "insert";
           } else {
@@ -253,31 +251,7 @@ this.$swal.fire(
         this.settinglist = [];
       }
     },
-    async deletesetting(data) {
-      const headers = {
-        Authorization: "Bearer " + this.userdetails.access_token,
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      };
-      const response = await this.$axios.post(
-        "/general-setting/remove",
-        {
-          added_by: this.userdetails.user.id,
-          setting_id: data.id,
-        },
-        { headers }
-      );
-      if (response.data.code == 200) {
-        this.$swal.fire('Deleted Successfully', '', 'success');
-        this.GetSettingList();
-      } else {this.$swal.fire({
-                  icon: 'error',
-                  title: 'Oops... Something Went Wrong!',
-                  text: 'the error is: ' + JSON.stringify(response.data.message),
-                  footer: ''
-                });
-      }
-    },
+ 
     async editsetting(data) {
       const headers = {
         Authorization: "Bearer " + this.userdetails.access_token,
@@ -295,6 +269,7 @@ this.$swal.fire(
         this.settingId = response.data.setting[0].id;
         this.gender = response.data.setting[0].section_value;
         this.index = response.data.setting[0].section_order;
+        this.status=response.data.setting[0].status;
         this.requesttype = "update";
       } else {
         this.$swal.fire({
