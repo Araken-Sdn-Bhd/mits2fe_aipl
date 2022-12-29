f37df9c5b6935c0e86964aa071cc5c294940ee2b
AcapXasyraF <acapxasyrafx@yahoo.com>
2022-12-29 18:43:29 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,17 @@ module.exports = {
     {
       name: 'MITS 2.0',
       exec_mode: 'cluster',
-      instances: '6', // Or a number of instances
+      instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start'
+      cron_restart: '0 0 * * *',
+			kill_timeout: 10000,
+			max_memory_restart: '512M',
+			autorestart: true,
+			out_file: './logs/service_core.log',
+			error_file: './logs/service_core.err',
+			log_date_format: 'YYYY-MM-DD HH:mm:ss SSS',
+      args: 'start',
+      watch: true,
     }
   ]
 }
